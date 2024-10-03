25fbd153c34d88d0fe718c8e9214c2b40e40d895
CuAnschutzShuaiZhu <shuai.zhu@cuanschutz.edu>
2024-10-03 17:09:47 -0600

    more on multi variable regression

--- Project1/Code/data_cleaning.R
+++ Project1/Code/data_cleaning.R
@@ -4,7 +4,8 @@ library(gtsummary)
 library(table1) 
 library(openxlsx)
 n = 430
-df_hiv <- read.csv('BIOS6624-shuai/Project1/DataRaw/hiv_6624_final.csv')%>%as_tibble()
+working_directory <- 'C:/Users/zhushu/OneDrive/Graduate File/Course/Bios6624/BIOS6624-shuai/Project1'
+df_hiv <- read.csv(paste0(working_directory, '/DataRaw/hiv_6624_final.csv'))%>%as_tibble()
 
 ### convert long format to wide format
 df_hiv_wider <- df_hiv%>%
@@ -24,22 +25,28 @@ df_hiv_lm_model <- df_hiv_wider%>%drop_na()%>%
   filter(BMI_year0<=200, BMI_year0>0)
 
 ### draw histogram 
-hist(df_hiv_lm_model$BMI_year0)
 
+hist(df_hiv_lm_model$BMI_year0)
 hist(df_hiv_lm_model$LEU3N_year0)
+
 ### transform vload and leu3n 
 df_hiv_lm_model <- df_hiv_lm_model%>%
   mutate(VLOAD_year0_log =log(VLOAD_year0),
          VLOAD_year2_log =log(VLOAD_year2),
          LEU3N_year0_log =log(LEU3N_year0),
-         LEU3N_year2_log =log(LEU3N_year2)
-         )
+         LEU3N_year2_log =log(LEU3N_year2))%>%
   ### categorize education variable
   mutate(edu = factor(EDUCBAS_year2, levels = 1:7, 
-                    labels = c(rep('High school', 3),'Some college','some college', 'Graduate, Post Graduate','Graduate, Post Graduate')))
+                    labels = c(rep('High school', 3),'Some college','some college', 'Graduate, Post Graduate','Graduate, Post Graduate')))%>%
+  ### categroize bmi variable
+  mutate(BMI = case_when(BMI_year0 < 18.5 ~ "Underweight",
+                         BMI_year0 > 18.5 & BMI_year0 < 25 ~ "Healthy",
+                         BMI_year0 > 25 & BMI_year0 < 30 ~ "Overweight",
+                         BMI_year0 > 30 ~ "Obsese"))%>%
+  select(-c('BMI_year0', 'BMI_year2', 'EDUCBAS_year2', 'hard_drugs_year2', 'age_year2', 'EDUCBAS_year0'))%>%
+  rename(Age = 'age_year0', Hard_drugs = 'hard_drugs_year0', Edu = edu, Adh = adh)
 
-
-write.xlsx(df_hiv_lm_model, 'BIOS6624-shuai/Project1/DataProcessed/cleaned data.xlsx')
+write.xlsx(df_hiv_lm_model, paste0(working_directory, '/DataProcessed/cleaned data.xlsx'))
 
 
 
--- Project1/Code/lm.R
+++ Project1/Code/lm.R
@@ -1,56 +1,51 @@
 library(openxlsx)
+working_directory <- 'C:/Users/zhushu/OneDrive/Graduate File/Course/Bios6624/BIOS6624-shuai/Project1'
 
-
-
-df_cleaned <- read.xlsx('BIOS6624-shuai/Project1/DataProcessed/cleaned data.xlsx')
-
+df_cleaned <- read.xlsx(paste0(working_directory, '/DataProcessed/cleaned data.xlsx'))
 df_hiv_lm_model <- df_cleaned
 
+outcome <- c('VLOAD_year2_log', 'LEU3N_year2', 'AGG_PHYS_year2', 'AGG_MENT_year2')
+predictors <- c('Hard_drugs', 'BMI', 'Age', 'Edu', 'Adh')
+baseline_outcome <- c('VLOAD_year0_log', 'LEU3N_year0', 'AGG_PHYS_year0','AGG_MENT_year0')
+
 ## univarite model
-df_hiv_lm_model$VLOAD_year0_log <- log(df_hiv_lm_model$VLOAD_year0)
-df_hiv_lm_model$VLOAD_year2_log <- log(df_hiv_lm_model$VLOAD_year2)
 
 df_hiv_lm_model%>%tbl_uvregression(
   method = lm,
   y = VLOAD_year2_log,
-  include = c('hard_drugs_year2', 'BMI_year2', 'age_year2', 'EDUCBAS_year2', 'VLOAD_year0_log', 'adh')
+  include = c('Hard_drugs', 'BMI', 'Age', 'Edu', 'VLOAD_year0_log', 'Adh')
 )%>%modify_header(label = "**VLOAD Model**")
 
 df_hiv_lm_model%>%tbl_uvregression(
   method = lm,
   y = LEU3N_year2,
-  include = c('hard_drugs_year2', 'BMI_year2', 'age_year2', 'EDUCBAS_year2', 'LEU3N_year0', 'adh')
+  include = c('Hard_drugs', 'BMI', 'Age', 'Edu', 'LEU3N_year0', 'Adh')
 )%>%modify_header(label = "**LEU3N Model**")
 
 df_hiv_lm_model%>%tbl_uvregression(
   method = lm,
   y = AGG_PHYS_year2,
-  include = c('hard_drugs_year2', 'BMI_year2', 'age_year2', 'EDUCBAS_year2', 'AGG_PHYS_year0', 'adh')
+  include = c('Hard_drugs', 'BMI', 'Age', 'Edu', 'AGG_PHYS_year0', 'Adh')
 )%>%modify_header(label = "**AGG_PHYS Model**")
 
 df_hiv_lm_model%>%tbl_uvregression(
   method = lm,
   y = AGG_MENT_year2,
-  include = c('hard_drugs_year2', 'BMI_year2', 'age_year2', 'EDUCBAS_year2', 'AGG_MENT_year0', 'adh')
+  include = c('Hard_drugs', 'BMI', 'Age', 'Edu', 'AGG_MENT_year0', 'Adh')
 )%>%modify_header(label = "**AGG_MENTModel**")
 
-##
-model.fit.vload <- lm(VLOAD_year2_log~hard_drugs_year0+EDUCBAS_year2+VLOAD_year0_log+adh,
-                      data = df_hiv_lm_model)
-summary(model.fit.vload)
+## multiple variable regression fit
 
-model.fit.LEU3N <- lm(LEU3N_year2~hard_drugs_year0+LEU3N_year0,
-                      data = df_hiv_lm_model)
-summary(model.fit.LEU3N)
+model_list = list()
+for (i in 1:4){
+  model.fit <- lm(paste0(outcome[i],'~', paste0(predictors, collapse ='+'),'+',baseline_outcome[i] ),
+                 data = df_hiv_lm_model)
+  model_list[[i]] <- model.fit
+}
 
-model.fit.AGG_PHYS <- lm(AGG_PHYS_year2~hard_drugs_year0+age_year2+AGG_PHYS_year0,
-                         data = df_hiv_lm_model)
-summary(model.fit.AGG_PHYS)
-
-model.fit.AGG_MENT <- lm(AGG_MENT_year2~hard_drugs_year0+EDUCBAS_year2+AGG_MENT_year0,
-                         data = df_hiv_lm_model)
-summary(model.fit.AGG_MENT)
-
-
+summary(model_list[[1]])
+summary(model_list[[2]])
+summary(model_list[[3]])
+summary(model_list[[4]])
 
 
