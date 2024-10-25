aceb4057a2ac08b6475dca61e2c8f244994fbe20
CuAnschutzShuaiZhu <shuai.zhu@cuanschutz.edu>
2024-10-25 14:27:41 -0600

    first version final report

--- Project2/Code/simulation.R
+++ Project2/Code/simulation.R
@@ -1,5 +1,9 @@
 
 
+## set the working directory
+working_directory <- 'C:/Users/zhushu/OneDrive/Graduate File/Course/Bios6624/BIOS6624-shuai/Project2'
+setwd(working_directory)
+## read cleaned data
 
 df_filtered <- readRDS(paste0(working_directory, '/DataProcessed/cleaned data.RDS'))
 glm( mortstat~gender, data = df_filtered)%>%summary()
