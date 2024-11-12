b590a53df4cad6ced7f3e63a69eafac7294be407
CuAnschutzShuaiZhu <shuai.zhu@cuanschutz.edu>
2024-11-12 15:41:04 -0700

    project3 modeling

--- Project3/Code/01_data_cleaning.R
+++ Project3/Code/01_data_cleaning.R
@@ -1,7 +1,7 @@
 
 ### set working directory
 
-working_directory <- 'C:/Users/zhu-s/OneDrive/Graduate File/Course/Bios6624/BIOS6624-shuai/Project3'
+working_directory <- 'C:/Users/zhushu/OneDrive/Graduate File/Course/Bios6624/BIOS6624-shuai/Project3'
 setwd(working_directory)
 ### load library
 library(tidyverse)
@@ -12,7 +12,7 @@ df <- readRDS('DataRaw/BIOS6624_proj2_data.rds')
 
 ## subset data
 
-df_cleaned <- df%>%select(SEQN,age_years_interview, n_good_days, permth_exm, diabetes, CHF, CHD, BMI_cat,gender, TMIMS_mean, L5_MIMS_mean, RA_MIMS_mean)%>%
+df_cleaned <- df%>%select(SEQN,age_years_interview, n_good_days, permth_exm, diabetes, CHF, CHD, BMI_cat, gender, TMIMS_mean, L5_MIMS_mean, RA_MIMS_mean,mortstat)%>%
   filter(age_years_interview>=40, age_years_interview<=80, n_good_days>=3)%>%drop_na()
 
 df_cleaned%>%saveRDS('DataProcessed/cleaned data.RDS')
--- Project3/Code/02_data_analysis.R
+++ Project3/Code/02_data_analysis.R
@@ -1,6 +1,29 @@
-
+library(caret)
 ## read cleaned data
 
 df_cleaned <- readRDS('DataProcessed/cleaned data.RDS')
 
 ## 
+
+
+mims_variable <- c('TMIMS_mean', 'L5_MIMS_mean', 'RA_MIMS_mean')
+risk_factors <- c("age_years_interview",  "diabetes", "CHF", "CHD", "BMI_cat" )
+outcome <- c("mortstat ", "permth_exm")
+
+## 
+
+model_fomula <- c(paste0('permth_exm~',mims_variable),
+ paste0('permth_exm~', paste0(mims_variable,'+', paste0(risk_factors, collapse = '+'))),
+ paste0('permth_exm~s(',mims_variable,')'),
+ paste0('permth_exm~',paste0('s(',mims_variable,')','+', paste0(risk_factors, collapse = '+')))
+ )
+
+train_control <- trainControl(method = "cv", number = 10)
+train(model_fomula[1], 
+               data = df_cleaned, 
+               method = "gam", 
+               trControl = train_control,
+               weights = df_cleaned$mortstat, 
+               family = cox.ph(), 
+               method = "REML")
+
