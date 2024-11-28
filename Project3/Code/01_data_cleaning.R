b71897bdf6c8e986c71054c4ef93711c3a94078f
ShuaiZ2020 <2317709047z@gmail.com>
2024-11-27 17:10:17 -0700

    final report generated

--- Project3/Code/01_data_cleaning.R
+++ Project3/Code/01_data_cleaning.R
@@ -1,11 +1,11 @@
 
 ### set working directory
 
-working_directory <- 'C:/Users/zhushu/OneDrive/Graduate File/Course/Bios6624/BIOS6624-shuai/Project3'
+working_directory <- 'C:/Users/zhu-s/OneDrive/Graduate File/Course/Bios6624/BIOS6624-shuai/Project3'
 setwd(working_directory)
 ### load library
 library(tidyverse)
-
+library(table1)
 ## read raw data
 
 df <- readRDS('DataRaw/BIOS6624_proj2_data.rds')
@@ -13,8 +13,30 @@ df <- readRDS('DataRaw/BIOS6624_proj2_data.rds')
 ## subset data
 
 df_cleaned <- df%>%select(SEQN,age_years_interview, n_good_days, permth_exm, diabetes, CHF, CHD, BMI_cat, gender, TMIMS_mean, L5_MIMS_mean, RA_MIMS_mean,mortstat)%>%
-  filter(age_years_interview>=40, age_years_interview<=80, n_good_days>=3)%>%drop_na()
+  filter(age_years_interview>=40, age_years_interview<=80, n_good_days>=3)%>%
+  mutate(mortstat = as.factor(mortstat))%>%drop_na()
+keep_id <- df_cleaned$SEQN
+df_table1 <- df%>%filter(age_years_interview>=40, age_years_interview<=80)%>%
+  mutate(drop = case_when(SEQN%in% keep_id~'Include',
+                                     .default = 'Exclude'),
+                         drop = factor(drop),
+                         drop = relevel(drop, ref = 'Include'))%>%
+  select(SEQN,age_years_interview, n_good_days, permth_exm, diabetes, CHF, CHD, BMI_cat, gender, TMIMS_mean, L5_MIMS_mean, RA_MIMS_mean,mortstat,drop)
+
+label(df_table1$age_years_interview) <- "Age"
+label(df_table1$n_good_days) <- "Number of good days "
+label(df_table1$permth_exm) <- "Months of follow-up"
+label(df_table1$diabetes) <- "Diabetes"
+label(df_table1$CHF) <- "congestive heart failure"
+label(df_table1$CHD) <- "coronary heart disease"
+label(df_table1$BMI_cat) <- "Body mass index"
+label(df_table1$TMIMS_mean) <- "Total MIMS"
+label(df_table1$L5_MIMS_mean) <- "L5 MIMS"
+label(df_table1$RA_MIMS_mean) <- "RA MIMS"
+label(df_table1$mortstat) <- "Mortality rate"
+
+table1(~TMIMS_mean+L5_MIMS_mean+RA_MIMS_mean+age_years_interview+n_good_days+permth_exm+diabetes+CHF+CHD+BMI_cat+mortstat|drop*gender, 
+       data = df_table1, overall = F, caption = 'Summary of NHANES dataset')%>%saveRDS(file = 'Figure/table1.RDS')
+
 
 df_cleaned%>%saveRDS('DataProcessed/cleaned data.RDS')
-
-
--- Project3/Code/02_data_analysis.R
+++ Project3/Code/02_data_analysis.R
@@ -1,30 +1,61 @@
 library(caret)
 library(mgcv)
+library(survival)
 ## read cleaned data
-
+set.seed(123)
 df_cleaned <- readRDS('DataProcessed/cleaned data.RDS')
 
-## 
-
-
+## outcome and predictors
 mims_variable <- c('TMIMS_mean', 'L5_MIMS_mean', 'RA_MIMS_mean')
 risk_factors <- c("age_years_interview",  "diabetes", "CHF", "CHD", "BMI_cat" )
 outcome <- c("mortstat ", "permth_exm")
 
-## 
+## define models 
 
 model_fomula <- c(paste0('permth_exm~',mims_variable),
  paste0('permth_exm~', paste0(mims_variable,'+', paste0(risk_factors, collapse = '+'))),
  paste0('permth_exm~s(',mims_variable,')'),
  paste0('permth_exm~',paste0('s(',mims_variable,')','+', paste0(risk_factors, collapse = '+')))
  )
+## divide data to male and female 
+df_male <- df_cleaned%>%filter(gender =='Male')
+df_female <- df_cleaned%>%filter(gender =='Female')
+folds.male <- createFolds(df_male$mortstat, k = 10, list = TRUE, returnTrain = TRUE)
+folds.female <- createFolds(df_female$mortstat, k = 10, list = TRUE, returnTrain = TRUE)
 
-train_control <- trainControl(method = "cv", number = 10)
-train(model_fomula[1], 
-      data = df_cleaned,
-      trControl = train_control,
-      method = "gam", 
-      weights = df_cleaned$mortstat, 
-      family = cox.ph(), 
-      method = "REML")
+cross_validation_gam <- function(folds, df_cleaned, fomula){
+  concordance <- c()
+  for(i in 1:10){
+    train <- df_cleaned[folds[[i]],]
+    test <- df_cleaned[-folds[[i]],]
+
+    model <- gam(as.formula(fomula), data = train, family = cox.ph(), weights = as.integer(mortstat))
+    pred <- predict(model, newdata = test)
+    concordance <- c(concordance, concordancefit(Surv(test$permth_exm, as.integer(test$mortstat)), x=pred, reverse=TRUE)$concordance )
+
+  }
+  return(mean(concordance))
+}
+
+
+model_concordance_male <-  sapply(model_fomula, function(x) cross_validation_gam(folds.male, df_male, x))
+model_concordance_female <-  sapply(model_fomula, function(x) cross_validation_gam(folds.female, df_female, x))
+model_concordance <- c('Uni TMIMS', 'Uni L5 MIMS', 'Uni RA TMIMS', 
+                       'Multi TMIMS', 'Multi L5 MIMS', 'Multi RA TMIMS',
+                       'Uni Non linear TMIMS', 'Uni Non linear L5 MIMS', 'Uni Non linear RA TMIMS',
+                       'Multi Non linear TMIMS', 'Multi Non linear L5 MIMS', 'Multi Non linear RA TMIMS')
+names(model_concordance_male) <- model_concordance
+names(model_concordance_female) <- model_concordance
+
+model_concordance_male_sort <- sort(model_concordance_male, decreasing  = T)
+model_concordance_female_sort <- sort(model_concordance_female, decreasing  = T)
+round_3 <- function(x) round(x, digits = 3)
+tibble(Rank = 1:12,
+       'Male Model' = names(model_concordance_male_sort),
+       'Male Concordance' = model_concordance_male_sort,
+       'Female Model' = names(model_concordance_female_sort),
+       'Female concordance' = model_concordance_female_sort)%>%
+  mutate_if(is.numeric, round_3)%>%
+  saveRDS('Figure/concordance table.RDS')
+
 
