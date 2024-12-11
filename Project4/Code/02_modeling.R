7e6c70728b166068983a445606acbf7409fa5782
Zhu <shuai.zhu@cuanschutz.edu>
2024-12-11 13:35:26 -0700

    model update

--- Project4/Code/02_modeling.R
+++ Project4/Code/02_modeling.R
@@ -1,8 +1,8 @@
-
+df_Q1 <- df_lmer
 ## fit the correct glmer model with random slope
-fit.glmm <- glmer(Y ~ trt*sin_s + (sin_s - 1|ID) + (cos_s - 1|ID), 
+fit_glmm <- glmer(Y ~ trt*sin_s + (sin_s - 1|ID) + (cos_s - 1|ID), 
                   family=poisson, data=df_Q1)
-summary(fit.glmm)
+
 
 ## save the random effect and beta
 s2_b1 <- data.frame(VarCorr(fit_glmm))$vcov[1]
@@ -22,7 +22,7 @@ sind_pred <- seq(0,1,len=n_spred)
 true_effect <- beta_vec[2] + beta_vec[4]*sin(2*pi*sind_pred)
 
 ## set up container for results
-nsim <- 5
+nsim <- 500
 results_arr <- 
   array(NA_real_, dim=c(nsim, n_spred, 3, 3),
         dimnames=list("simulation"=1:nsim,
@@ -39,19 +39,22 @@ df_sim <-
          cos_s = cos(4*pi*sind))
 
 
-store_results <- function(Xmat, vbeta_hat, model_name){
+store_results <- function(res,Xmat, vbeta_hat,eta_hat, model_name,i){
   SE_correct_i <- sqrt(diag(Xmat %*% vbeta_hat %*% t(Xmat)) )
-  LB_correct_i <- eta_hat_correct_i - qnorm(0.975)*SE_correct_i
-  UB_correct_i <- eta_hat_correct_i + qnorm(0.975)*SE_correct_i
+  LB_correct_i <- eta_hat - qnorm(0.975)*SE_correct_i
+  UB_correct_i <- eta_hat + qnorm(0.975)*SE_correct_i
   
   ## store results
-  results_arr[i,,model_name,1] <- (eta_hat_correct_i - true_effect)^2
-  results_arr[i,,model_name,2] <- eta_hat_correct_i - true_effect
-  results_arr[i,,model_name,3] <- as.numeric(LB_correct_i < true_effect & UB_correct_i > true_effect)
+  res[i,,model_name,1] <- (eta_hat - true_effect)^2
+  res[i,,model_name,2] <- eta_hat - true_effect
+  res[i,,model_name,3] <- as.numeric(LB_correct_i < true_effect & UB_correct_i > true_effect)
+  res
 }
 
 ## progress bar
 pb <- txtProgressBar(0, nsim, style=3)
+set.seed(123)
+#for(i in 1:nsim){
 for(i in 1:nsim){
   ## simulate data
   bi1_i <- rnorm(N, mean=0, sd=sqrt(s2_b1))
@@ -83,17 +86,18 @@ for(i in 1:nsim){
                 rep(0, n_spred),
                 sin(2*pi*sind_pred))
   vbeta_hat_correct_i <- vcov(fit_correct_i)
-  store_results(Xmat, vbeta_hat_correct_i, "correct")
+  results_arr <- store_results(results_arr, Xmat, vbeta_hat_correct_i, eta_hat_correct_i, "correct",i)
   ### misspecified random effect model
-
+  eta_hat_misspecified_re_i <- fit_misspecified_re_i@beta[2] + fit_misspecified_re_i@beta[4]*sin(2*pi*sind_pred)
   vbeta_hat_misspecified_re_i <- vcov(fit_misspecified_re_i)
-  store_results(Xmat, vbeta_hat_misspecified_re_i, "misspecified_re")
+  results_arr <- store_results(results_arr, Xmat, vbeta_hat_misspecified_re_i, eta_hat_misspecified_re_i,"misspecified_re",i)
   ### misspecified mean random effect model
   Xmat_no_inter <- cbind(rep(0, n_spred), 
-                rep(1, n_spred),
-                rep(0, n_spred))
+                         rep(1, n_spred),
+                         rep(0, n_spred))
+  eta_hat_misspecified_mean_re_i <- fit_misspecified_re_i@beta[2]
   vbeta_hat_misspecified_mean_re_i <- vcov(fit_misspecified_mean_re_i)
-  store_results(Xmat_no_inter, vbeta_hat_misspecified_re_i, "misspecified_mean_re")
+  results_arr <- store_results(results_arr, Xmat_no_inter, vbeta_hat_misspecified_mean_re_i,eta_hat_misspecified_mean_re_i, "misspecified_mean_re",i)
   
   setTxtProgressBar(pb,i)
 }
