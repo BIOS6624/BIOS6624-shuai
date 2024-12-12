75381e3f89538eb757a5ae9fd62404d48b67aa36
Zhu <shuai.zhu@cuanschutz.edu>
2024-12-12 12:43:26 -0700

    model modified back

--- Project4/Code/01_data_cleaning.R
+++ Project4/Code/01_data_cleaning.R
@@ -1,10 +1,10 @@
-
+rm(list = ls())
 
 library(tidyverse);library(lme4);library(parallel)
 
 
 ## set working directory
-working_directory <- 'C:/Users/zhu-s/OneDrive/Graduate File/Course/Bios6624/BIOS6624-shuai/Project4'
+working_directory <- 'C:/Users/zhushu/OneDrive/Graduate File/Course/Bios6624/BIOS6624-shuai/Project4'
 setwd(working_directory)
 
 df <- read.csv('DataRaw/MVPA_seasonal.csv')%>%as_tibble()
--- Project4/Code/02_modeling.R
+++ Project4/Code/02_modeling.R
@@ -1,9 +1,11 @@
-
-
 df_Q1 <- df_lmer
 ## fit the correct glmer model with random slope
-fit_glmm <- glmer(Y ~ trt*sin_s + (sin_s - 1|ID) + (cos_s - 1|ID), 
-                  family=poisson, data=df_Q1)
+if(file.exists('DataProcessed/fit_glmm.RDS')){
+  fit_glmm <- readRDS('DataProcessed/fit_glmm.RDS')
+}else{
+  fit_glmm <- glmer(Y ~ trt*sin_s + (sin_s - 1|ID) + (cos_s - 1|ID), family=poisson, data=df_Q1)
+  saveRDS(fit_glmm,'DataProcessed/fit_glmm.RDS')
+}
 
 
 ## save the random effect and beta
@@ -40,25 +42,29 @@ df_sim <-
   mutate(sin_s = sin(2*pi*sind),
          cos_s = cos(4*pi*sind))
 
-
-store_results <- function(Xmat, vbeta_hat,eta_hat){
+### define a function to store model result to results_arr
+store_results <- function(results_arr,fit, model_name,i){
+  
+  eta_hat <- fit@beta[2] + fit@beta[4]*sin(2*pi*sind_pred)
+  vbeta_hat <- vcov(fit)
   SE_correct_i <- sqrt(diag(Xmat %*% vbeta_hat %*% t(Xmat)) )
   LB_correct_i <- eta_hat - qnorm(0.975)*SE_correct_i
   UB_correct_i <- eta_hat + qnorm(0.975)*SE_correct_i
-  res <- matrix(rep(NA, 3000), ncol = 3)
-  ## store results
   
-  res[,1] <- (eta_hat - true_effect)^2
-  res[,2] <- eta_hat - true_effect
-  res[,3] <- as.numeric(LB_correct_i < true_effect & UB_correct_i > true_effect)
-  res
+  ## store results
+  results_arr[i,,model_name,1] <- (eta_hat - true_effect)^2
+  results_arr[i,,model_name,2] <- eta_hat - true_effect
+  results_arr[i,,model_name,3] <- as.numeric(LB_correct_i < true_effect & UB_correct_i > true_effect)
+  results_arr
 }
 
 ## progress bar
-
 set.seed(123)
+start <- Sys.time()
+pb <- txtProgressBar(0, nsim, style=3)
+
 #for(i in 1:nsim){
-one_interation <- function(i){
+for(i in 1:nsim){
   ## simulate data
   bi1_i <- rnorm(N, mean=0, sd=sqrt(s2_b1))
   bi2_i <- rnorm(N, mean=0, sd=sqrt(s2_b2))
@@ -71,59 +77,25 @@ one_interation <- function(i){
   df_i$Y <- vapply(df_i$eta_ij, function(x) rpois(1, exp(x)),numeric(1))  
   
   ## fit the models
-  fit_correct_i <- glmer(Y ~ trt*sin_s + (sin_s - 1|ID) + (cos_s - 1|ID), 
-                         family=poisson, data=df_i)
-  eta_hat_correct_i <- fit_correct_i@beta[2] + fit_correct_i@beta[4]*sin(2*pi*sind_pred)
+  fit_correct_i <- glmer(Y ~ trt*sin_s + (sin_s - 1|ID) + (cos_s - 1|ID), family=poisson, data=df_i)
+  fit_misspecified_re_i <- glmer(Y ~ trt*sin_s + (1+sind|ID), family=poisson, data=df_i)
+  fit_misspecified_mean_re_i <- glmer(Y ~ trt*sind + (1+sind|ID), family=poisson, data=df_i)
+  ## define Xmat
   Xmat <- cbind(rep(0, n_spred), 
                 rep(1, n_spred),
                 rep(0, n_spred),
                 sin(2*pi*sind_pred))
-  vbeta_hat_correct_i <- vcov(fit_correct_i)
-  res_correct <- store_results( Xmat, vbeta_hat_correct_i, eta_hat_correct_i )
-  
-  ### misspecified random effect model
-  fit_misspecified_re_i <- glmer(Y ~ trt*sin_s + (1+sind|ID) , 
-                                 family=poisson, data=df_i)
+  ## get estimated coefficients
+  # \hat{treatment effect}(s)
 
-  eta_hat_misspecified_re_i <- fit_misspecified_re_i@beta[2] + fit_misspecified_re_i@beta[4]*sin(2*pi*sind_pred)
-  vbeta_hat_misspecified_re_i <- vcov(fit_misspecified_re_i)
-  
-  res_misspecified_re <- store_results(Xmat, vbeta_hat_misspecified_re_i, eta_hat_misspecified_re_i)
-  
-  ### misspecified mean random effect model
-  fit_misspecified_mean_re_i <- glmer(Y ~ trt*sind + (1+sind|ID), 
-                                      family=poisson, data=df_i)
-  eta_hat_misspecified_mean_re_i <- fit_misspecified_mean_re_i@beta[2]+ fit_misspecified_mean_re_i@beta[4]*sin(2*pi*sind_pred)
-  vbeta_hat_misspecified_mean_re_i <- vcov(fit_misspecified_mean_re_i)
-  res_misspecified_mean_re <- store_results(Xmat, vbeta_hat_misspecified_mean_re_i, eta_hat_misspecified_mean_re_i)
-  
-  res_matrix <- cbind(res_correct, res_misspecified_re, res_misspecified_mean_re)
-  return(res_matrix)
+  # test something
+  ### correct model
+  results_arr <- store_results(results_arr, fit_correct_i, 'correct', i)
+  results_arr <- store_results(results_arr, fit_misspecified_re_i,"misspecified_re",i)
+  results_arr <- store_results(results_arr, fit_misspecified_mean_re_i, "misspecified_mean_re",i)
+
+  setTxtProgressBar(pb,i)
 }
-### multithread simulation
-a <- one_interation()
-cl <- makeCluster(detectCores())
-clusterEvalQ(cl,{
-  set.seed(1234)
-  library(dplyr)
-  library(lme4)
-  NULL
-})
-clusterExport(cl, "N")
-clusterExport(cl, "sind_pred")
-clusterExport(cl, "s2_b1")
-clusterExport(cl, "s2_b2")
-clusterExport(cl, "beta_vec")
-clusterExport(cl, "df_sim")
-clusterExport(cl, "n_spred")
-clusterExport(cl, "true_effect")
-clusterExport(cl, "store_results")
-clusterExport(cl, "one_interation")
-
-start <- Sys.time()
-res_list <- parLapply(cl, 1:4, one_interation)
 end <- Sys.time()
 end-start
-
-
-
+saveRDS(results_arr, 'DataProcessed/results_arr.RDS')
